b89a1e48ad3dc87695386980b524433ebf756f05
fuat özer <fuatozerr23@gmail.com>
2019-07-22 20:28:47 +0300

    Created entities

---
+++ Project/Project.Northwind.Entities/Concreate/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Northwind.Entities.Concreate
+{
+   public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ Project/Project.Northwind.Entities/Concreate/Product.cs
@@ -0,0 +1,16 @@
+﻿using Project.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Northwind.Entities.Concreate
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
