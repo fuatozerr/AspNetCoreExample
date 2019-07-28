017bc37c24a7d86d3741f6289928daad5f2f8b38
fuat özer <fuatozerr23@gmail.com>
2019-07-28 14:52:57 +0300

    view components and i writed services in startup

---
+++ Project/Project.MvcWebUI/Models/CategoryListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Northwind.Entities.Concreate;
+
+namespace Project.MvcWebUI.Models
+{
+    public class CategoryListViewModel
+    {
+        public List<Category> categories;
+    }
+}
---
+++ Project/Project.MvcWebUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Project.Business.Abstract;
+using Project.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.MvcWebUI.ViewComponents
+{
+    public class CategoryListViewComponent:ViewComponent
+    {
+        private ICategoryService _categoryService;
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            var model = new CategoryListViewModel
+            {
+                categories = _categoryService.GetAll()
+            };
+            return View(model);
+            
+        }
+    }
+}
